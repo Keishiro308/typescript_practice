c8a4e88f1548ccf82c27669237bd349bf0aee964
Keishiro308 <kn11308@gmail.com>
2024-05-02 21:10:08 +0900

    第二章途中までコミット

---
+++ src/chapter2/program_2_3.ts
@@ -0,0 +1,21 @@
+const binary = 0b1010;
+
+const octal = 0o755;
+
+const hexademical = 0xff;
+
+const big = 1e8;
+
+const small = 4e-5;
+
+console.log(binary, octal, hexademical, big, small);
+
+const bignum: bigint = (123n + 456n) * 2n;
+
+console.log(bignum);
+
+const backquote_message: string = `Hello, 
+
+World!`;
+
+console.log(backquote_message);
